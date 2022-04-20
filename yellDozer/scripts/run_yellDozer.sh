8bb624639d0cf79fea5be2a1efcce2db97acd272
CyberTDan <rbtcreator@yandex.ru>
2022-04-20 11:13:06 +0300

    Add changes for wifi node

--- yellDozer/scripts/run_yellDozer.sh
+++ yellDozer/scripts/run_yellDozer.sh
@@ -4,7 +4,8 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 source /catkin_ws/devel/setup.bash
 set -e
 
-echo DEV_PATH_SYMLINK:$DEV_PATH_SYMLINK
-./../../scripts/uavcan_tools/create_slcan_from_serial.sh $DEV_PATH_SYMLINK
+# echo DEV_PATH_SYMLINK:$DEV_PATH_SYMLINK
+# ./../../scripts/uavcan_tools/create_slcan_from_serial.sh $DEV_PATH_SYMLINK
+./../../scripts/uavcan_tools/create_slcan_from_udp.sh &
 export PYTHONUNBUFFERED=1
 roslaunch yellDozer yelldozer_test.launch
